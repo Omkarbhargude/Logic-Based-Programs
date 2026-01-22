48cadd8ebf6c6ed777eb9add80ce2068d9b89358
Omkar Mahadev Bhargude <omkarbhargude29@gmail.com>
2026-01-22 22:50:33 +0530

    Delete Java_programming/display.java

--- Java_programming/display.java
+++
@@ -1,9 +0,0 @@
-//Problem-Statement :- write a function which displays Jay Ganesh on console
-
-class display
-{
-    public static void main(String A[])
-    {
-        System.out.println("Jay Ganesh...");
-    }
-}
